dfae5228c44b94d3488570f7a1cb4f45559bb728
kaviavarthini <kavivarthini@gmail.com>
2026-02-02 14:38:28 +0530

    Added backend server

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-require("dotenv").config();
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
 
+dotenv.config(); // Initialize dotenv
 const app = express();
 
 // --- 1. MIDDLEWARE ---
